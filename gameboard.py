f476d495a79871c0cfd839e7f05f39e3b702792d
Marco Dominguez <marcod082@gmail.com>
2023-08-04 16:57:34 -0500

    finished game board

--- gameboard.py
+++ gameboard.py
@@ -1,8 +1,17 @@
+from enum import Enum
+class win_status(Enum):
+    IN_PROGRESS = 0
+    WIN = 1
+    DRAW = 2
+
+class player(Enum):
+    P1 = 1
+    p2 = 2
 
 class tictactoe_board:
     def __init__(self):
         self.board = [[1,2,3], [4,5,6], [7,8,9]]
-        self.floor = '{:-^15}'.format('')
+        self.floor = '{:-^13}'.format('')
         self.wall = '{:^5}'.format('|') 
         self.BOARD_DIMENSION = 3
 
@@ -11,26 +20,23 @@ class tictactoe_board:
             print(f"{self.board[i][0]}{self.wall}{self.board[i][1]}{self.wall}{self.board[i][2]}")
             if i != self.BOARD_DIMENSION - 1:
                 print(self.floor)
-
-    
-
+                
 class tictactoe_game:
     def __init__(self):
         self.game_board = tictactoe_board()
         self.symbol = {'P1':'X', 'P2':'O'}
-        self. win_status = False
+        self. win_status = win_status.IN_PROGRESS
+        self.current_player = player.P1
         self.valid_coord_choices = {1:(0,0), 2:(0,1), 3:(0,2), #all valid locations on game board
                                     4:(1,0), 5:(1,1), 6:(1,2),
                                     7:(2,0), 8:(2,1), 9:(2,2)}
-        #NOTE might add a player attribute
-        #NOTE might make current_player an enum type
 
-    def place_symbol(self, current_player, location_choice):
+    def place_symbol(self, location_choice):
         
         if location_choice in self.valid_coord_choices:
             row, col = self.valid_coord_choices[location_choice] #store the row and col that corresponds with selected location 
             try: #incase if current_player has an invalid value
-                current_symbol = self.symbol[current_player]     #choose the current symbol depending on the current player
+                current_symbol = self.symbol[self.current_player]     #choose the current symbol depending on the current player
                 self.game_board.board[row][col] = current_symbol #assign the current symbol to the selected location on game board
                 self.valid_coord_choices.pop(location_choice)    #when a location is used remove it as a possible valid location
 
@@ -42,7 +48,6 @@ class tictactoe_game:
             print("Invalid board location") #NOTE might change so it returns this string
 
     def list_same(self,item_list):
-        #all_same = True
         if item_list[0] != 'X' and item_list[0] != 'O':
             return False
         
@@ -53,26 +58,41 @@ class tictactoe_game:
         
         return True
     
-    #TODO implement check col func
-    #TODO implement check row func
-    #TODO implement check diag func
+    def check_column(self):
+        for i in range(self.game_board.BOARD_DIMENSION):
+            col_list = [c[i] for c in self.game_board.board]
+            if self.list_same(col_list):
+                return True
+        
+        return False
+        
+    def check_row(self):
+        for row in self.game_board.board:
+            if self.list_same(row):
+                return True
+        return False
+    
+    def check_diagonal(self):
+        diag1 = []
+        for i in range(self.game_board.BOARD_DIMENSION):
+            diag1.append(self.game_board.board[i][i])
+        
+        if self.list_same(diag1):
+            return True
+        
+        diag2 = []
+        for r in range(self.game_board.BOARD_DIMENSION):
+            c = self.game_board.BOARD_DIMENSION - 1 - r
+            diag2.append(self.game_board.board[r][c])
+        
+        if self.list_same(diag2):
+            return True
+        
+        return False
 
     def check_winner(self):
-        pass
-
-
-
-t = tictactoe_game()
-
-
-
-
-
-
-
-
-
-
-
-
+        if self.check_column() or self.check_row or self.check_diagonal:
+            self.win_status = win_status.WIN
+        elif len(self.valid_coord_choices) == 0:
+            self.win_status = win_status.DRAW
 
